f2fcde52b80ec8f04dc2ce188ccf71e27190ebef
Sangam8168 <144759674+Sangam8168@users.noreply.github.com>
2023-09-23 19:42:30 +0530

    Update Assign1_Q6.cpp

--- Assign1_Q6.cpp
+++ Assign1_Q6.cpp
@@ -8,12 +8,12 @@ int main(){
     cin >> n;
 
     int i = 0;
-    int x = 1;
+    int pos = 1;
     while (n > 0){
         int digit = n % 10;
         n = n / 10;
-        i += x * pow(10, digit - 1);
-        x++;
+        i += pos * pow(10, digit - 1);
+        pos++;
     }
 
     cout <<i<< endl;
